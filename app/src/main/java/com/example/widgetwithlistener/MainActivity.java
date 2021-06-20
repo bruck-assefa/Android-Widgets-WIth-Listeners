b3d09786e2eb638e3e60062c8f18c349feb0c6fe
bruckassefa <sirblackness@gmail.com>
2021-06-19 22:22:22 -0400

    Initial Commit and Push

---
+++ app/src/main/java/com/example/widgetwithlistener/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.widgetwithlistener;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final int redColor = 0xff595e;
+    private final int blueColor = 0x1982c4;
+    private final int greenColor = 0x8ac926;
+    private final int yellowColor = 0xffca3a;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
